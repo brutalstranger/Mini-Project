5d3643763d8cfd00e9a8c719c9cd5a38b0076ceb
brutalstranger <yoavamir@gmail.com>
2017-06-26 14:26:18 +0300

    changesss

--- sentence_filter.py
+++ sentence_filter.py
@@ -10,7 +10,7 @@ def get_output_path_of_txt_file(original_path):
     split = re.split(r"\\", original_path)
     end = split.pop()
     pre_end = split.pop()
-    output_txt = r"C:\Users\yoav\Desktop\ben_yehuda\outputs" + "\\" + str(pre_end) + "\\" + str(end)
+    output_txt = r"C:\Users\yoav\Desktop\ben_yehuda\try" + "\\" + str(pre_end) + "\\" + str(end)
     return output_txt
 
 
@@ -18,7 +18,7 @@ def get_output_dir_of_txt_file(original_path):
     split = re.split(r"\\", original_path)
     split.pop()
     pre_end = split.pop()
-    output_txt = r"C:\Users\yoav\Desktop\ben_yehuda\outputs" + "\\" + str(pre_end)
+    output_txt = r"C:\Users\yoav\Desktop\ben_yehuda\try" + "\\" + str(pre_end)
     return output_txt
 
 
@@ -44,18 +44,36 @@ def filter_beginning(input_dir , unsuccessful_list):
                             if sentence_to_filrer in line:
                                 found_sentence = 1
                 except Exception as e:
-                    list.append(original_file_path)
+                    unsuccessful_list.append(original_file_path)
                     print str(e)
+
+def remove_letochen(output_dir):
+    for subdir, dirs, files in os.walk(output_dir):
+        for filename in files:
+            file_path = os.path.join(output_dir , subdir , filename)
+            print file_path
+            with open(file_path , 'r') as f:
+                lines = f.readlines()
+                f.close()
+                g = open(file_path, "w")
+                for line in lines:
+                    if line != tochen + "\n":
+                        g.write(line)
+                g.close()
+
 
 
 if __name__ == '__main__':
+    output_dir = r"C:\Users\yoav\Desktop\ben_yehuda - Copy"
     orig_stdout = sys.stdout
-    unsuccessful_list = []
+    # unsuccessful_list = []
     sentence_to_filrer = "יומן הרשת של פרויקט בן-יהודה"
+    tochen = "לתוכן הענינים"
+
+
     input_dir = r"C:\Users\yoav\Downloads\benyehuda_sep2016_dump_with_nikkud_utf8"
-    filter_beginning(input_dir=input_dir , unsuccessful_list=unsuccessful_list)
+    # filter_beginning(input_dir=input_dir , unsuccessful_list = unsuccessful_list)
+    remove_letochen(output_dir)
     sys.stdout.close()
     sys.stdout = orig_stdout
-    print list
-
-#FFFFFFFFFFFFffcccc
+    # print unsuccessful_list
