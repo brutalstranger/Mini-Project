4295ed046d6c81499003a02a5748bb13a3ef3f4a
giladhosh <g_hosh@yahoo.co.uk>
2017-06-26 18:05:05 +0300

    Add files via upload

---
+++ src/js/scripts.js
@@ -0,0 +1,19 @@
+// Empty JS for your own code to be here
+
+
+
+function test() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+
+
+
+function runSearch() {
+    document.getElementById("demo").innerHTML = "Function not implemented yet";
+    /**
+     * TODO:
+     * 1. Search for data related to first and second words
+     * 2. Create Graph 
+     * 3. Add data of both words to graph
+     **/
+}
