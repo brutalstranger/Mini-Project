5fe83f333a226ed5f1a8fc0e91722da513f04734
giladhosh <g_hosh@yahoo.co.uk>
2017-08-13 22:40:48 +0300

    Add files via upload

--- searcher.php
+++ searcher.php
@@ -3,22 +3,31 @@ error_reporting(E_ALL);  //debug
 ini_set("display_errors", 1); //debug
 
 
+//$string = preg_replace('/\s/', '', $string);
+
 $word1 = strval($_GET['searchWord1']);
 $word2 = strval($_GET['searchWord2']);
 $chart_type = strval($_GET['chart']);
+$ngram = strval($_GET['ngram']);
 
 $searchWords = array();
 $sql  = array();
-
-
 $number_of_search_terms = 0;
-if($word1 != ""){
-	$searchWords[$number_of_search_terms] = $word1;
-	$number_of_search_terms++; 
+if($ngram){
+	$ngram_string = $word1;
+	$ngram_string = preg_replace('/\s/', '', $ngram_string); //remove whitespace
+	$searchWords = explode( "," , $ngram_string ); //split into array using "," as delimiter
+	$number_of_search_terms = count($searchWords);
 }
-if($word2 != ""){
-	$searchWords[$number_of_search_terms] = $word2;
-	$number_of_search_terms++;
+else{
+	if($word1 != ""){
+		$searchWords[$number_of_search_terms] = $word1;
+		$number_of_search_terms++; 
+	}
+	if($word2 != ""){
+		$searchWords[$number_of_search_terms] = $word2;
+		$number_of_search_terms++;
+	}
 }
 
 $db_host        = 'localhost';
@@ -34,7 +43,7 @@ $max_results 	= '31';
  */
 for($a = 0; $a<$number_of_search_terms; $a++)
 {
-	$sql[$a] = "";
+	$sql[$a] = ""; 
 	if($chart_type == "sunburstChart")
 	{
 		$sql[$a] = "SELECT * FROM ".$db_table." WHERE author = '".$searchWords[0]."' ORDER BY (count) DESC LIMIT ".$max_results.";" ; // search term will always be first and only
@@ -58,24 +67,26 @@ if(!$status)
 	die('searcher.php: mysqli_select_db failed!');
 
 // change to dynamic init
-$array_of_results = array(	0 => array(),
-							1 => array()); //init results array
+$array_of_results = array(); //array(	0 => array(), 1 => array()); //init results array
+
+//$a = array(); // array of columns
+for($c=0; $c<$number_of_search_terms; $c++){
+    $array_of_results[$c] = array(); // array of cells for column $c
+    //for($r=0; $r<3; $r++){
+   //     $a[$c][$r] = rand();
+    //}
+}
 
 $rowcount = 0;
 for($i = 0 ; $i<$number_of_search_terms ; $i++){
 	$result = mysqli_query($con,$sql[$i]); 
-	//if($i)
-	//	$result = mysqli_query($con,$sql2); // i = 1
-	//else
-		//$result = mysqli_query($con,$sql1); // i = 0
 	$data_array = mysqli_fetch_all($result , MYSQLI_ASSOC); //mysqli_fetch_array($result , MYSQLI_NUM);
 	$array_of_results[$i] = $data_array;
 
-$rowcount += mysqli_num_rows($result); //query error check
- if($rowcount == 0 && $i == ($number_of_search_terms-1)){ //no results from all queries
-	$array_of_results =  new stdClass();
-        //echo ('No results, sql1 = '.$sql[0].", sql2 = ".$sql[1]);
-    }
+	$rowcount += mysqli_num_rows($result); //query error check
+	 if($rowcount == 0 && $i == ($number_of_search_terms-1)){ //no results from all queries
+		$array_of_results =  new stdClass(); //return empty object
+		}
 }
 
 mysqli_free_result($result);
